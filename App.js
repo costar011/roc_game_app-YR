f483568f9c6249ccac59bf8b0e783ef68a664aa6
YERIM <dnaer456@gmail.com>
2021-03-09 13:29:20 +0900

    tools

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   StyleSheet,
   Text,
@@ -8,13 +8,44 @@ import {
 } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
 
+const CURRENT_WIDTH = Dimensions.get(`window`).width;
+
 const App = () => {
-  return <View style={styles.container}></View>;
+  const [tab, setTab] = useState(0);
+
+  const _startButtonClickHandler = (value) => {
+    setTab(value);
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.backgroundArea}></View>
+      <View style={styles.resultArea}></View>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+
+  backgroundArea: {
+    flex: 8,
+
+    alignItems: `center`,
+    justifyContent: `center`,
+
+    backgroundColor: `#6ccaf0`,
+  },
+
+  resultArea: {
+    flex: 2,
+
+    alignItems: `center`,
+    justifyContent: `center`,
+
+    backgroundColor: `#4e9bed`,
   },
 });
 
